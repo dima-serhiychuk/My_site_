12c69297af35c5369ddc34cfc9ede148a14e703c
dima-serhiychuk <91623052+dima-serhiychuk@users.noreply.github.com>
2022-08-28 19:02:17 +0300

    s

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function changeTheame () {
     }   
 }
 
+document.querySelector('.numberC').innerText = decodeURIComponent(cookieValue);
+
 function setVal() {
     var cookieValue = document.getElementById('inp').value;
     document.cookie = encodeURIComponent(cookieValue);
     alert(document.cookie);
     document.querySelector('.numberC').innerText = decodeURIComponent(cookieValue);
 }
-
-document.querySelector('.numberC').innerText = decodeURIComponent(cookieValue);
